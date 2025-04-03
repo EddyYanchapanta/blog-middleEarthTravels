1aeaf4b76c7dacf7a07083f955ef4d1513e3854e
Eddy Yanchapanta <eddy.yanchapanta@outlook.com>
2025-04-03 15:26:30 +0200

    add routes, components, interfaces and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { ViewPostComponent } from './pages/view-post/view-post.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'new', component: NewPostComponent },
+  { path: 'post/:idpost', component: ViewPostComponent },
+  { path: '**', redirectTo: 'home' },
+];
